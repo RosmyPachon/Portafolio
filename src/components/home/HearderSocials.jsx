0b15faef5eee9da990dafdcbc52c8842e1dd19f3
RosmyPachon <rosmypachon@gmail.com>
2024-01-31 17:30:29 -0500

    feat(detalles home)

---
+++ src/components/home/HearderSocials.jsx
@@ -0,0 +1,22 @@
+import "./home.css"
+
+const HearderSocials = () => {
+  return (
+    <div className="home__socials">
+      <a href="" className="home__social-link" target="_blanck">
+        <i class="fa-brands fa-instagram"></i>
+      </a>
+      <a href="" className="home__social-link" target="_blanck">
+        <i class="fa-brands fa-x-twitter"></i>
+      </a>
+      <a href="" className="home__social-link" target="_blanck">
+        <i class="fa-brands fa-github"></i>
+      </a>
+      <a href="" className="home__social-link" target="_blanck">
+        <i class="fa-brands fa-discord"></i>
+      </a>
+    </div>
+  );
+};
+
+export default HearderSocials;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,27 @@
-import "./home.css"
-import Me from "../../assets/avatar-1.svg"
+import "./home.css";
+import Me from "../../assets/avatar 1.png";
+import HearderSocials from "./HearderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
 
 const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
+        <Shapes />
         <img src={Me} alt="" className="home__img" />
         <h1 className="home__name">Rosmy Pachón</h1>
-        <span className="home__education"></span>
+        <span className="home__education">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </span>
+        <HearderSocials />
+        <a href="#contact" className="btn">
+          algo ahí
+        </a>
+        <ScrollDown />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
